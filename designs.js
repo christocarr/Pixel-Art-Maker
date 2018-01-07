eaad8bb5d7ba5e53f6598814b9e221dd87536b1b
Chris Carr <christocarr@outlook.com>
2018-01-07 12:26:51 +0000

    added mouse click on cell functionality
    todo clear cell and color multiple cells when mouse is gragged over cells.

--- designs.js
+++ designs.js
@@ -2,43 +2,41 @@ $(document).ready(function() {
 	// Select color input
 	// Select size input
 
-	const submit = $(':submit');
-	const gridLimit = 30;
-	const gridTable = $('#pixel_canvas');
+	const SUBMIT = $(':submit');
+	const GRID_LIMIT = 30;
+	const GRID_TABLE = $('#pixel_canvas');
 
-	submit.click(function(event) {
+	SUBMIT.click(function(event) {
 		event.preventDefault();
-		const gridHeight = parseInt($('#input_height').val());
-		const gridWidth = parseInt($('#input_width').val());
-		makeGrid(gridHeight, gridWidth);
+		const GRID_HEIGHT = parseInt($('#input_height').val());
+		const GRID_WIDTH = parseInt($('#input_width').val());
+		makeGrid(GRID_HEIGHT, GRID_WIDTH);
 	});
 
 	// When size is submitted by the user, call makeGrid()
 
-	function makeGrid(gridHeight, gridWidth) {
+	function makeGrid(GRID_HEIGHT, GRID_WIDTH) {
 		let grid;
 		
-		if (gridHeight <= gridLimit && gridWidth <= gridLimit){
+		if (GRID_HEIGHT <= GRID_LIMIT && GRID_WIDTH <= GRID_LIMIT){
 			
-			for (let x = 0; x < gridWidth; ++x) {
+			for (let x = 0; x < GRID_WIDTH; ++x) {
 				grid += '<tr>';
-				for (let y = 0; y < gridHeight; y++) {
+				for (let y = 0; y < GRID_HEIGHT; y++) {
 					grid += '<td></td>';
 				}
 				grid += '</tr>'
 			}
-			gridTable.html(grid);
+			GRID_TABLE.html(grid);
 			
-		} else {
-			gridTable.html('Grid height and width is 30 max!. Please adjust the input.');
+		} else { //warning to user grid is oversized
+			GRID_TABLE.html('Grid height and width is 30 max!. Please adjust the input.');
 		}
 	}
 	
-	const tableCell = $('.tableCell');
-	
-	gridTable.on('mousedown', function(event){
-		event.stopPropagation;
-		const color = $('#colorPicker').val();
+	//set the background color of the table cell when 
+	GRID_TABLE.on('mousedown', 'td', function(event){
+		$(this).css('background-color', $('#colorPicker').val());
 	});
 							 
 })
