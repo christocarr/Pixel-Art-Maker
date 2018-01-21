615d206e81d90f6758d67afcccc33b7358308aff
Chris Carr <christocarr@outlook.com>
2018-01-21 19:26:12 +0000

    Added grid height and width properties to the grid object

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const GRID = {
+	gridHeight: function() {
+		let gridHeight = document.getElementById('input_height');
+		gridHeight = gridHeight.valueAsNumber;
+		return gridHeight;
+	},
+	
+	gridWidth: function() {
+		let gridWidth = document.getElementById('input_width');
+		gridWidth = gridWidth.valueAsNumber;
+		return gridWidth;
+	},
+	
+	gridCanvas: function() {
+		const GRID_CANVAS = document.getElementById('pixel_canvas');
+		return GRID_CANVAS;
+	},
+	
 	makeGrid: function(height, width) {
-		let canvas = document.getElementById('pixel_canvas');
 		let grid = '';
 		for (let i = 0; i < height; i++) {
 			grid += '<tr>';
@@ -9,8 +25,9 @@ const GRID = {
 			}
 		}
 		grid += '</tr>';
-		canvas.innerHTML = grid;	
+		GRID.gridCanvas().innerHTML = grid;
 	},
+	
 	cellColor: function() {
 		let color = document.getElementById('colorPicker').value;
 		return color;
@@ -20,11 +37,8 @@ const GRID = {
 const HANDLERS = {
 	addGrid: function(event){
 		event.preventDefault();
-		let gridWidth = document.getElementById('input_width');
-		let gridHeight = document.getElementById('input_height');
-		gridHeight = gridHeight.valueAsNumber;
-		gridWidth = gridWidth.valueAsNumber;
-		GRID.makeGrid(gridHeight, gridWidth);
+
+		GRID.makeGrid(GRID.gridHeight(), GRID.gridWidth());
 		HANDLERS.colorGrid();
 		
 	},
@@ -39,7 +53,7 @@ const HANDLERS = {
 	},
 	
 	clearGrid: function() {
-		
+		GRID.gridCanvas().style.backgroundColor = 'none';
 	}
 	
 };
